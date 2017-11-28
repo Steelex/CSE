42c2a32f14554000ef9b2073707e8e2183146e9b
Steelex <31050349+Steelex@users.noreply.github.com>
2017-11-28 15:07:15 -0800

    Adding Notes
    11.27.17

---
+++ Notes-Steelex.py
@@ -0,0 +1,30 @@
+print("Hello World")
+
+# i like pickles
+
+print(3+5)
+print(5-3)
+print(5*3)
+print(6/2)
+print(3 ** 2)
+
+print() # Creates a blank line
+print("See if you can figure this out")
+print(15 % 5)
+
+# Variables
+car_name = "Mobile Legends"
+car_type = "Tesla Model 3"
+car_cylinders = 8
+car_mpg = 9000.1
+
+#inline printing
+print("My car is the %s." % car_name)
+print("My car is the %s. It is a %s" % (car_name, car_type))
+
+# Taking Input
+name = input("What is your name?")
+print("Hello %s." % name)
+#print(name)
+age = input("What is your age? ")
+print("%s?! That old? You belong in a museum.")
