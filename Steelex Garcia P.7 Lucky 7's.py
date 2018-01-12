d4a4712358f21027db15e83a728dbd19cf93ee6e
Steelex <31050349+Steelex@users.noreply.github.com>
2018-01-12 11:16:37 -0800

    1/12/18
    Lucky's 7

--- Steelex Garcia P.7 Lucky 7's.py
+++ Steelex Garcia P.7 Lucky 7's.py
@@ -3,11 +3,11 @@ import random
 # Period 7
 
 print()
-
+broke = False
 money = 15
 played = 0
-
-while money > 0:
+high = 0
+while money > 0 and broke is False :
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
     total = dice1 + dice2
@@ -17,4 +17,9 @@ while money > 0:
         money += 4
     else:
         money -= 1
-print("Rounds played", played)
+    if high < money:
+        high = money
+        thing = round
+    if money == 0:
+        broke = True
+        print("Rounds played", played)
