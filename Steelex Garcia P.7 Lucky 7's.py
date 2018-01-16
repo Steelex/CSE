fc7ed4bb1db9dee4635367019fd7bd6ada8266ee
Steelex <31050349+Steelex@users.noreply.github.com>
2018-01-16 10:27:37 -0800

    1/16/18
    Work

--- Steelex Garcia P.7 Lucky 7's.py
+++ Steelex Garcia P.7 Lucky 7's.py
@@ -1,25 +1,26 @@
 import random
 # Steelex Garcia
-# Period 7
+# Period 3
 
 print()
 broke = False
 money = 15
 played = 0
 high = 0
-while money > 0 and broke is False :
+while money > 0 and broke is False:
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
     total = dice1 + dice2
     print("You rolled a", total)
-    played += 1
     if total == 7:
         money += 4
+        played += 1
     else:
         money -= 1
+        played += 1
     if high < money:
         high = money
-        thing = round
+        numb = played
     if money == 0:
         broke = True
         print("Rounds played", played)
