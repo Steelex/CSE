eaf028ddafaeedbcedad3b68ecd5e6262d9dfba5
Steelex <31050349+Steelex@users.noreply.github.com>
2018-01-23 10:46:31 -0800

    1/23/18

--- Hangman - Steelex Garcia Period 3.py
+++ Hangman - Steelex Garcia Period 3.py
@@ -5,12 +5,16 @@ import random
 A general guide for Hangman
 1. Make a word bank - 10 items
 2. Pick a random word from the item from the list
-3. Hide the word (use*)
+3. Add a guess to the list of letters guessed
 4. Reveal letters already guessed
 5. Create the win condition
 """
 word_bank = ["A step ahead of cataclysm", "Metal is perfection", "I feel your fear", "Looks can be deceiving",
              "Consume and adapt", "Unmatched Power", "Imagine if I had a real weapon", "We are as one",
              "Trample their bones", "No pain no drain"]
-
+correct_phase = False
 lives = 10
+
+while lives < 0 and correct_phase is False:
+    random.shuffle(word_bank)
+print(word_bank)
