2e2f3aedb5064c397ce9a905b0a0f0fc27215ca0
Steelex <31050349+Steelex@users.noreply.github.com>
2018-01-24 10:46:47 -0800

    1/24/18

--- Hangman - Steelex Garcia Period 3.py
+++ Hangman - Steelex Garcia Period 3.py
@@ -12,9 +12,12 @@ A general guide for Hangman
 word_bank = ["A step ahead of cataclysm", "Metal is perfection", "I feel your fear", "Looks can be deceiving",
              "Consume and adapt", "Unmatched Power", "Imagine if I had a real weapon", "We are as one",
              "Trample their bones", "No pain no drain"]
-correct_phase = False
+correct_phrase = False
 lives = 10
-
-while lives < 0 and correct_phase is False:
-    random.shuffle(word_bank)
-print(word_bank)
+start = True
+letters_guessed = [""]
+while lives > 0 and correct_phrase is False:
+        random.shuffle(word_bank)
+        word = word_bank.pop(7)
+        word = (list(word))
+        print(word)
