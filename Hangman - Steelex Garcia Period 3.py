339388bb8f7262294d6c4e74ef9f83a1296561f0
Steelex <31050349+Steelex@users.noreply.github.com>
2018-01-29 10:46:22 -0800

    1/29/18

--- Hangman - Steelex Garcia Period 3.py
+++ Hangman - Steelex Garcia Period 3.py
@@ -10,23 +10,32 @@ A general guide for Hangman
 4. Reveal letters already guessed
 5. Create the win condition
 """
-word_bank = ["A step ahead of cataclysm", "Metal is perfection", "I feel your fear", "Looks can be deceiving",
-             "Consume and adapt", "Unmatched Power", "Imagine if I had a real weapon", "We are as one",
-             "Trample their bones", "No pain no drain"]
-lives = 10
-word = (random.choice(word_bank))
-word = (list(word))
-length = len(output)
-print(word)
-letters = ["abcdefghijklmnopqrstuvwxyz"]
-words_guessed = []
-while lives > 0:
-    guess = input("Guess a letter or try the whole word. ")
-    if guess == word:
-        print("Good Job You Did It!")
-    if guess == word:
-        print("Good Job")
-    if guess != word:
-        lives -= 1
-        print("You have %s guesses left" % lives)
-
+word_bank = ["a step ahead of cataclysm", "metal is perfection", "i feel your fear", "looks can be deceiving",
+             "consume and adapt", "unmatched power", "imagine if i had a real weapon", "we are as one",
+             "trample their bones", "no pain no drain"]
+alphabet = list(string.ascii_lowercase)
+random_word = random.choice(word_bank)
+correct = list(random_word)
+print("This is Hangman. Each of my guesses are phrases are from LOL. Can you guess them? You have 10 tries.")
+letters_guessed = []
+tries = 10
+while tries > 0:
+    output = []
+    for letter in random_word:
+        if letter in letters_guessed:
+            output.append(letter)
+        else:
+            output.append("*")
+    print(output)
+    tries -= 1
+    if output == correct:
+        print("You Win Good Job XD!")
+        exit(0)
+    print("You can guess these letters:), %s" % alphabet)
+    ask_for_letter = input("Guess a letter or even the whole word.")
+    lowercase_guess = ask_for_letter.lower()
+    letters_guessed.append(lowercase_guess)
+    if lowercase_guess in alphabet:
+        alphabet.remove(lowercase_guess)
+    if tries == 0:
+        print("Game Over. The words are %s" % random_word)
