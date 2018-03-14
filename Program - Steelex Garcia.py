b7b2b72e1bb727a191817ae42d019818479f2788
Steelex <31050349+Steelex@users.noreply.github.com>
2018-03-14 10:20:34 -0700

    3/14/18

--- Program - Steelex Garcia.py
+++ Program - Steelex Garcia.py
@@ -6,6 +6,7 @@ class Person(object):
     def work(self):
         print("%s goes to work" % self.name)
 
+
 class Employee(Person):
     def __init__(self, name, age, job, description):
         super(Employee, self).__init__(name, age)
@@ -13,7 +14,8 @@ class Employee(Person):
         self.description = description
 
     def resume(self):
-        print("Employee Name: %s, Employee Age: %s, Employee Description: %s" % self.name % self.age % self.description)
+        print("Employee Name: %s, Employee Age: %s, Employee Description: %s" % (self.name, self.age, self.description))
+
 
 class Programmer(Person):
     def __init__(self, name, age, pay):
@@ -21,4 +23,10 @@ class Programmer(Person):
         self.pay = pay
 
     def program(self):
-        print("%s has made a program" % self.name)
+        print("%s has successfully finished a program." % self.name)
+
+
+franco = Employee("Franco", 27, "Cashier", "A short and asian male.")
+franco.resume()
+kindra = Programmer("Kindra", "20", 4500)
+kindra.program()
